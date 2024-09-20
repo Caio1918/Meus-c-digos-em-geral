d6b8af8103cd910a85c46228976e9fba0f2eadb0
Caio Onha Ferreira <caioonha@gmail.com>
2024-09-20 20:30:12 -0300

    Variáveis com valores importantes

--- Estatísca/ex7d.py
+++ Estatísca/ex7d.py
@@ -5,3 +5,11 @@ import matplotlib.pyplot as plt
 
 dados = pd.Series([1595,1472,1820,1580,1804,1635,1959,2020,1480,1250,2083,1522,1306,1572,1778,2296,1445,1716,1618,1824])
 
+# valores importantes
+total = dados.count()
+v_max = dados.max()
+v_min = dados.min()
+amp = v_max - v_min
+qtde_classes = 8
+h = ceil(amp/qtde_classes)
+
