645d5e188a655156b83c43192cffc9a94ddac728
Caio Onha Ferreira <caioonha@gmail.com>
2024-09-20 20:06:05 -0300

    Inicio da tabela de frequência

--- Estatísca/ex7c.py
+++ Estatísca/ex7c.py
@@ -5,3 +5,18 @@ import matplotlib.pyplot as plt
 
 dados = pd.Series([507,389,305,291,336,310,514,442,373,428,387,454,323,441,388,426,411,382,320,450,309,416,359,388,307,337,469,351,422,413])
 
+# Valores importantes
+total = dados.count()
+v_max = dados.max()
+v_min = dados.min()
+amp = v_max - v_min
+qtde_classes = 8
+h = ceil(amp/qtde_classes)
+
+freq = dados.value_counts(bins=qtde_classes).sort_index()
+df = pd.DataFrame(freq)
+df = df.reset_index()
+df.columns = ["Classe", "Frequência"]
+
+
+print(df)
