6b006e769eae812d0bda0873c43f88de4fa3b64e
Caio Onha Ferreira <caioonha@gmail.com>
2024-09-20 20:27:41 -0300

    Iniciando ex7d e adicionando os dados

---
+++ Estatísca/ex7d.py
@@ -0,0 +1,7 @@
+from math import sqrt, ceil
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+dados = pd.Series([1595,1472,1820,1580,1804,1635,1959,2020,1480,1250,2083,1522,1306,1572,1778,2296,1445,1716,1618,1824])
+
