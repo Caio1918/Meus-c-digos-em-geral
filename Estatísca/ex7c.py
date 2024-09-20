7b8b79aca14ecf08b512da96c6a98686f091c42a
Caio Onha Ferreira <caioonha@gmail.com>
2024-09-20 20:01:55 -0300

    Importando bibliotecas e adicinando os dados

---
+++ Estatísca/ex7c.py
@@ -0,0 +1,7 @@
+from math import sqrt, ceil
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+dados = pd.Series([507,389,305,291,336,310,514,442,373,428,387,454,323,441,388,426,411,382,320,450,309,416,359,388,307,337,469,351,422,413])
+
