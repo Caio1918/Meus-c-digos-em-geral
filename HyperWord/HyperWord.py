2b3d55b34f1555b04a6e3304c9af55103fd2e837
Caio Onha Ferreira <caioonha@gmail.com>
2024-09-26 22:55:35 -0300

    Atualizando o HyperWord

--- HyperWord/HyperWord.py
+++ HyperWord/HyperWord.py
@@ -1,50 +1,19 @@
-from time import time
+def Hyperword():
+    word = [".",".",".","."]
 
-alfabeto = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-palavra = ["a", "a", "a", "a"]
-
-l1 = 0; l2 = 1; l3 = 2; l4 = 3  # Cada variável representa um indice da lista palvra (l1 = letra 1)
-
-percorre_alfabeto1 = 0    # Cada índice da lista palavra necessita de uma variável que percorra o alfabeto 
-percorre_alfabeto2 = 0
-percorre_alfabeto3 = 0
-percorre_alfabeto4 = 0
-
-escolha = list("arte")
-
-tempo_inicial = time()  # Contador de tempo
-
-cont = 0
-while cont < 52**4:
-    palavra[l1] = alfabeto[percorre_alfabeto1]
-    print(palavra)
-    percorre_alfabeto1 += 1
-    cont += 1
-
-    if percorre_alfabeto1 > 51:
-        palavra[l2] = alfabeto[percorre_alfabeto2]
-        percorre_alfabeto2 += 1
-        percorre_alfabeto1 = 0
-
-        if percorre_alfabeto2 > 51:
-            palavra[l3] = alfabeto[percorre_alfabeto3]
-            percorre_alfabeto3 += 1
-            percorre_alfabeto2 = 0
-
-            if percorre_alfabeto3 > 51:
-                palavra[l4] = alfabeto[percorre_alfabeto4]
-                percorre_alfabeto4 += 1
-                percorre_alfabeto3 = 0
-                
-                if percorre_alfabeto4 > 51:
-                    break
-
+    for i in range(26):
+        word[0] = chr(97 + i)
+        print(word)
+        for j in range(26):
+            word[1] = chr(97 + j)
+            print(word)
+            for k in range(26):
+                word[2] = chr(97 + k)
+                print(word)
+                for l in range(26):
+                    word[3] = chr(97 + l)
+                    print(word)
     
-            
-tempo_final = time()    # Fim do contador de tempo
+    return word
 
-total = tempo_final -tempo_inicial
-
-print(f"{total:.10f}\n")
-print(cont)
-        
+print(Hyperword())
