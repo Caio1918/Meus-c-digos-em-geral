8aab30ea672d69c52dc0cdb96f742b739b3585dc
Caio Onha Ferreira <caioonha@gmail.com>
2024-10-10 16:25:09 -0300

    Criando a função do botão de cadastro

--- Desenvolvimento_Front_End/api_node/front/JS/sctipt.js
+++ Desenvolvimento_Front_End/api_node/front/JS/sctipt.js
@@ -19,3 +19,40 @@ async function obterFilmes() {
         celulaSinopse.innerHTML = filme.sinopse 
     }
 }
+
+async function cadastrarFilme() {
+
+    // Contrói a URL completa
+    const URLCompleta = `${protocolo}${baseURL}${filmesEndpoint}`
+
+    // Pega os inputs que contém os valores que o usuário digitou
+    let tituloInput = document.querySelector("#tituloInput")
+    let sinopseInput = document.querySelector("#sinopseInput")
+
+    // Pega os valores digitados pelo usuário
+    let titulo = tituloInput.value 
+    let sinopse = sinopseInput.value
+
+    // Limpa os campos que o usuário digitou
+    tituloInput.value = ""
+    sinopseInput.value = ""
+
+    // Envia os dados ao servidor (back end)
+    const filmes = (await axios.post(URLCompleta, {titulo, sinopse})).data
+
+    // Limpa a tabela para preenchê-la com a coleção nova, atualizada
+    let tabela = document.querySelector(".filmes")
+    let corpoTabela = tabela.getElementsByTagName("tbody")[0]
+    corpoTabela.innerHTML = ""
+
+    for (let filme of filmes) {
+
+        let linha = corpoTabela.innerRow(0)
+
+        let celulaTitulo = linha.insertCell(0)
+        let celulaSinopse = linha.insertCell(1)
+        
+        celulaTitulo.innerHTML = filme.titulo
+        celulaSinopse.innerHTML = filme.sinopse
+    }
+}
