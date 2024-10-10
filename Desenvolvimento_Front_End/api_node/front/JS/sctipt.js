b6d1bb5c394f4a345058d57e656c6008c0479e57
Caio Onha Ferreira <caioonha@gmail.com>
2024-10-10 15:55:58 -0300

    Adicionando linhas de filme e sipnopse ao front end

--- Desenvolvimento_Front_End/api_node/front/JS/sctipt.js
+++ Desenvolvimento_Front_End/api_node/front/JS/sctipt.js
@@ -5,5 +5,17 @@ const filmesEndpoint = "/filmes"
 async function obterFilmes() {
     const URLCompleta = `${protocolo}${baseURL}${filmesEndpoint}`
     const filmes = (await axios.get(URLCompleta)).data
-    console.log(filmes)
+
+    let tabela = document.querySelector(".filmes")
+    let corpoTabela = tabela.getElementsByTagName("tbody")[0]
+
+    for (let filme of filmes) {
+
+        let linha = corpoTabela.insertRow(0)
+        let celulaTitulo = linha.insertCell(0)
+        let celulaSinopse = linha.insertCell(1)
+
+        celulaTitulo.innerHTML = filme.titulo 
+        celulaSinopse.innerHTML = filme.sinopse 
+    }
 }
