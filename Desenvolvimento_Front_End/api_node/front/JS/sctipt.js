fb5000936bfaefa41676ed5193b22330fea21c3f
Caio Onha Ferreira <caioonha@gmail.com>
2024-10-10 16:45:29 -0300

    Alerta para preencher todos os campos

--- Desenvolvimento_Front_End/api_node/front/JS/sctipt.js
+++ Desenvolvimento_Front_End/api_node/front/JS/sctipt.js
@@ -15,8 +15,8 @@ async function obterFilmes() {
         let celulaTitulo = linha.insertCell(0)
         let celulaSinopse = linha.insertCell(1)
 
-        celulaTitulo.innerHTML = filme.titulo 
-        celulaSinopse.innerHTML = filme.sinopse 
+        celulaTitulo.innerHTML = filme.titulo
+        celulaSinopse.innerHTML = filme.sinopse
     }
 }
 
@@ -30,29 +30,44 @@ async function cadastrarFilme() {
     let sinopseInput = document.querySelector("#sinopseInput")
 
     // Pega os valores digitados pelo usuário
-    let titulo = tituloInput.value 
+    let titulo = tituloInput.value
     let sinopse = sinopseInput.value
 
-    // Limpa os campos que o usuário digitou
-    tituloInput.value = ""
-    sinopseInput.value = ""
+    //  Somente adiciona se o usuário tiver digitado os dois valores
+    if (titulo && sinopse) {
 
-    // Envia os dados ao servidor (back end)
-    const filmes = (await axios.post(URLCompleta, {titulo, sinopse})).data
+        // Limpa os campos que o usuário digitou
+        tituloInput.value = ""
+        sinopseInput.value = ""
 
-    // Limpa a tabela para preenchê-la com a coleção nova, atualizada
-    let tabela = document.querySelector(".filmes")
-    let corpoTabela = tabela.getElementsByTagName("tbody")[0]
-    corpoTabela.innerHTML = ""
+        // Envia os dados ao servidor (back end)
+        const filmes = (await axios.post(URLCompleta, { titulo, sinopse })).data
 
-    for (let filme of filmes) {
+        // Limpa a tabela para preenchê-la com a coleção nova, atualizada
+        let tabela = document.querySelector(".filmes")
+        let corpoTabela = tabela.getElementsByTagName("tbody")[0]
+        corpoTabela.innerHTML = ""
 
-        let linha = corpoTabela.innerRow(0)
+        for (let filme of filmes) {
 
-        let celulaTitulo = linha.insertCell(0)
-        let celulaSinopse = linha.insertCell(1)
-        
-        celulaTitulo.innerHTML = filme.titulo
-        celulaSinopse.innerHTML = filme.sinopse
+            let linha = corpoTabela.insertRow(0)
+
+            let celulaTitulo = linha.insertCell(0)
+            let celulaSinopse = linha.insertCell(1)
+
+            celulaTitulo.innerHTML = filme.titulo
+            celulaSinopse.innerHTML = filme.sinopse
+        }
+    }
+
+    // Senão, exibe o alerta por até 2 segundos
+    else {
+        let alert = document.querySelector('.alert')
+        alert.classList.add('show')
+        alert.classList.remove('d-none')
+        setTimeout(() => {
+            alert.classList.remove('show')
+            alert.classList.add('d-none')
+        }, 2000)
     }
 }
